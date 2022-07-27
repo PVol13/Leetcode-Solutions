fc339b05632b3176836b776904e90b402631c0d0
PVol13 <108609894+PVol13@users.noreply.github.com>
2022-07-27 22:29:51 +0530

    Time: 289 ms (42.54%), Space: 15.6 MB (65.09%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.py
+++ 283-move-zeroes/283-move-zeroes.py
@@ -3,13 +3,9 @@ class Solution:
         i = 0  #keep track of zeroes
         j = 0  #keep track of elements in the list
         
-        while j < len(nums):
+        for j in range (len(nums)):
             if nums[j] != 0:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-                j += 1
-            else:
-                j += 1
-        
         return nums
         
